5a5297786f9dc4dbb65ecbd74f40b285315ca19c
Vaibhav Pundir <94843746+VAIBHAV-PUNDIR2000@users.noreply.github.com>
2022-07-22 05:42:37 +0530

    sanity setup commit

---
+++ front_end/src/Wrapper/Wrapper.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { SocialMediaButtons, NavigationBubble } from "../Components";
+
+const Wrapper = () => {
+  return <div>Wrapper</div>;
+};
+
+export default Wrapper;
